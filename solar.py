423a324f966b07553bc657ff4ddfe177e29e6569
root <root@localhost.localdomain>
2015-12-07 04:14:17 +0000

    Faltan graficas

--- solar.py
+++ solar.py
@@ -1,9 +1,21 @@
 """ DESCRIPCION DEL PROGRAMA, PURPOSE, MIS DATOS"""
 
-import sys, string
+import sys, string, math
 
 # Verify if the user gives the file name via terminal, if not print the correct
 # way to use the program.
+
+def mean(d):
+	avg = 0
+	for key,value in d.items():
+		avg += value
+	return avg / len(d)
+
+def sd(d, avg):
+	s = 0
+	for key,value in d.items():
+		s += pow(value - avg,2)
+	return math.sqrt(s/len(d))
 
 if(len(sys.argv) < 2):
 	print("Usage: solar.py <filename>",file=sys.stderr)
@@ -21,9 +33,7 @@ try:
 				line = line.rstrip()
 				temp = line.split(' ') 
 				#First column is the day number and second the recording
-				dicReadings[int(temp[0])] = float(temp[1])
-	
-		
+				dicReadings[int(temp[0])] = float(temp[1])	
 except:
 	print("File Problem", file=sys.stderr)
 	exit(2)
@@ -36,20 +46,15 @@ for key in list(dicReadings.keys())[1:]:
 	if(key == it):
 		genElec[it] = dicReadings[it] - dicReadings[it-1]
 	#In this case there is no measure for the day so we find an avg for the amount generated
-	elif(it != key):
-		for i in range(it,key):
+	else:
+		for i in range(it,key+1):
 			genElec[i] = (dicReadings[key] - dicReadings[it-1])/ (key - (it - 1))
 		it = key	
 	it += 1
 
-
 #Print mean amount generated per day
 print("\nMean amount generated:")
-mean = 0
-for key,value in genElec.items():
-	mean += value	
-mean = mean / max(genElec.keys())	
-print(mean)
+print(mean(genElec))
 
 #Print min amount generated and day/s
 print("\nMinimum amount generated: (day, amount)")
@@ -64,3 +69,28 @@ maxValue = max(genElec.values())
 for key,value in genElec.items():
 	if(maxValue == value):
 		print(key,value)
+
+#Split into chunks of 30 days and calculate mean and std dev per chunk
+listGen = []
+d= {}
+c=0
+for key,value in genElec.items():
+	d[key] = value
+	c += 1
+	if(c == 30):
+		listGen.append(d)
+		d={}
+		c=0
+	if(key == len(genElec)):
+		listGen.append(d)
+
+#print mean and stdeviation per group of 30:
+print("\n--------Mean and Std.Deviation pero chunk of 30 data--------")
+c = 1
+for i in listGen:
+	print("\nChunk %d" % c)
+	print("Mean: %f" % mean(i))
+	print("Standard Deviation: %f" % sd(i,mean(i)))
+	c += 1
+#for i in listGen:
+#	print("\n",i)	
